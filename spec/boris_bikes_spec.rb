c9bdf6e02fc58db6b84d8cb20a743bd1e8fde6fc
MrJons <shad_stick@hotmail.co.uk>
2016-08-16 12:21:55 +0100

    DockingStation initialized and first test passed (basic).

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,7 +1,7 @@
 require 'boris_bikes'
 
 describe "boris bikes" do
-  it " should recognise DockingStation when instantiated" do
-    expect(DockingStation).to exist
+  it "should recognise DockingStation when instantiated" do
+    expect(DockingStation).to eq DockingStation
   end
 end
