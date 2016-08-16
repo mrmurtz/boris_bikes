20f23c3ad554ff941b450faaa7e58de7ee225cbd
MrJons <shad_stick@hotmail.co.uk>
2016-08-16 14:41:46 +0100

    Bike working test written, currently failing.

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,13 +3,17 @@ require 'boris_bikes'
 docking_station = DockingStation.new
 
 describe "boris bikes" do
-  it "should recognise DockingStation when instantiated" do
-    expect(DockingStation).to eq DockingStation
-  end
+
   it "when asking DockingStation to release_bike, a bike is released" do
     expect(docking_station).to respond_to(:release_bike)
   end
+
   it "when asking docking_station to release bike, we get 'Bike'" do
     expect(docking_station.release_bike).to eq "bike"
   end
+
+  it "When asking if bike is working, passes true." do
+    expect(working(bike)).to eq true
+  end
+
 end
