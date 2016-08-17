dc6675640270e2131f76182efbf137cf3d44685a
mrmurtz <murtaza.s.abidi@gmail.com>
2016-08-17 12:26:58 +0100

    Completed step 10, tests passing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,20 +1,13 @@
 require 'docking_station'
 
-docking_station = DockingStation.new
-bike = docking_station.release_bike
-
-describe "docking station" do
+describe DockingStation do
 
   it "when asking DockingStation to release_bike, a bike is released" do
-    expect(docking_station).to respond_to(:release_bike)
-  end
-
-  it "When asking if bike is working, passes true." do
-    expect(docking_station.working(bike)).to eq true
+    expect(subject).to respond_to(:release_bike)
   end
 
   it "released working bikes" do
-    bike = docking_station.release_bike
+    bike = subject.release_bike
     expect(bike).to be_working
   end
 end
